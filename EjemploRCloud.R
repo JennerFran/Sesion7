f55587ee46547ba25bb1460169f5f9967d4b6120
Jenner Franco <jennerfranco2@gmail.com>
2024-01-25 01:59:58 +0000

    Nuevo cambio

--- EjemploRCloud.R
+++ EjemploRCloud.R
@@ -3,10 +3,14 @@ hist(x)
 
 # This is a random code
 
-# Token: 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
+# Token: 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
 
 
 length(x)
 sum(x^2)
 sum(x^3)*sum(x^4)
 x+x
+
+# Otro comentario
+
+log(x^2)
